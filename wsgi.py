cbabb0132ef4f2bde14f04a05c36ca70b2cc80ce
Simon Reap <simon@simonreap.com>
2017-10-03 17:17:18 +0100

    Non-HTML

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,8 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<html><head><title>My Hello World</head><body>Hello World!</body></html>"
+    #return "<html><head><title>My Hello World</head><body>Hello World!</body></html>"
+    return "Forked My Hello World!"
 
 if __name__ == "__main__":
     application.run()
