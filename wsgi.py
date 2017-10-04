dfcd22e363a59c9c2ca8e7c00ad8011bcfb86882
Simon Reap <simon@simonreap.com>
2017-10-04 12:26:35 +0100

    Added level 2 header

--- wsgi.py
+++ wsgi.py
@@ -3,7 +3,7 @@ application = Flask(__name__)
 
 @application.route("/")
 def hello():
-    return "<html><head><title>Forked Python Build</title></head><body><h1>Forked My Hello World!</h1>Body text</body></html>"
+    return "<html><head><title>Forked Python Build</title></head><body><h1>Forked My Hello World!</h1><h2>Body text</h2></body></html>"
 
 if __name__ == "__main__":
     application.run()
